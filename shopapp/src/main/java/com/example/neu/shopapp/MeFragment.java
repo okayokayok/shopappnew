b8b01e8182aea1d7e55e131d89b4bf95293ad696
okayokayok <904360842@qq.com>
2017-10-09 01:08:04 +0800

    respository hello

--- shopapp/src/main/java/com/example/neu/shopapp/MeFragment.java
+++ shopapp/src/main/java/com/example/neu/shopapp/MeFragment.java
@@ -121,7 +121,7 @@ public class MeFragment extends Fragment implements Response.Listener<JSONObject
                     }
                 }else{
                     Toast.makeText(MeFragment.this.getActivity(),"抱歉，您还没有任何订单！",Toast.LENGTH_LONG).show();
-                //hello
+                //commit pull push
 
                 }
             }
